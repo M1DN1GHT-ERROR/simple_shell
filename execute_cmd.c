90a7c826112e56111c09a4121c066a14f2d1713e
imane <ilagsoun021@gmail.com>
2023-05-24 16:26:03 -0700

    memory

--- execute_cmd.c
+++ execute_cmd.c
@@ -7,49 +7,11 @@
 * @path_var: The value of the PATH environment variable
 */
 
-/*void exect_cmd(char *cmd, char **argv, char *path_var)
-{
-char *fin, *item;
-*Tokenize the command into arguments*/
-/*char **args = malloc((BUFFER_SIZE + 1) * sizeof(char *));
-int arg_count = 0;
-
-*Trim leading whitespace*
-while (isspace(*cmd))
-{
-cmd++;
-}*/
-
-/*Trim trailing whitespace*/
-/*fin = cmd + strlen(cmd) - 1;
-while (fin > cmd && isspace(*fin))
-{
-fin--;
-}
-*(fin + 1) = '\0';
-item = strtok(cmd, " ");
-while (item != NULL)
-{
-
-args[arg_count++] = item;
-item = strtok(NULL, " ");
-}
-args[arg_count] = NULL;
-
-if (is_builtin_cmvd(args[0]))
-execute_builtin_cmvd(args);
-else
-execut_exter_cmd(args[0], args, path_var, argv);
-
-free(args);
-free(path_var);
-}*/
-
 void exect_cmd(char *cmd, char **argv, char *path_var)
 {
     char *fin, *item;
     char **args = malloc((BUFFER_SIZE + 1) * sizeof(char *));
-    int arg_count = 0;
+    int arg_count = 0, i;
 
     while (isspace(*cmd))
     {
@@ -76,6 +38,12 @@ void exect_cmd(char *cmd, char **argv, char *path_var)
     else
         execut_exter_cmd(args[0], args, path_var, argv);
 
+    for (i = 0; i < BUFFER_SIZE +1; i++)
+	    if (args[i] != NULL)
+	    {
+		    free (args[i]);
+		    args[i] = NULL;
+		}
     free(args);
     if (path_var != NULL)
         free(path_var);
