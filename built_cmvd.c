d161cd48dc30987b2ddb001c99dbbcd388051f20
imane <ilagsoun021@gmail.com>
2023-05-23 17:11:49 -0700

    shell

--- built_cmvd.c
+++ built_cmvd.c
@@ -1,7 +1,7 @@
 #include "my_shell.h"
 
 /**
- * is_builtin_cmd - Checks if a command is a built-in command
+ * is_builtin_cmvd - Checks if a command is a built-in command
  * @command: The command to check
  * lagsimane
  * Return: 1 if the command is a built-in command, 0 otherwise
